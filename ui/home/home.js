2e61d7d2642775fbe89016820e30d1db11d6d3df
Divyansh Chaudhary <chaudharydivyansh1253@gmail.com>
2026-07-02 13:05:07 +0530

    Added some animations and did some improvements

---
+++ ui/home/home.js
@@ -0,0 +1,37 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("navbar-scrolled");
+    }
+    else{
+        navbar.classList.remove("navbar-scrolled");
+    }
+
+});
+
+const numbers = document.querySelectorAll(".fall-number");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            setTimeout(() => {
+                entry.target.classList.add("show");
+            }, 200);
+
+            observer.unobserve(entry.target);
+        }
+
+    });
+
+}, {
+    threshold: 0.9
+});
+
+numbers.forEach((number) => {
+    observer.observe(number);
+});
