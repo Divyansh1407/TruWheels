5a187c8ed0b8afcdac411d393897b083ce23471e
Divyansh Chaudhary <chaudharydivyansh1253@gmail.com>
2026-07-04 16:07:10 +0530

    Added uthentication Guard with responsive login model

--- ui/home/home.js
+++ ui/home/home.js
@@ -1,3 +1,9 @@
+import { supabase } from "../../supabase/supabaseClient.js";
+
+const loginModal = document.getElementById("loginModal");
+const continueGoogleBtn = document.getElementById("continueGoogleBtn");
+const cancelLoginBtn = document.getElementById("cancelLoginBtn");
+
 window.addEventListener("scroll", () => {
 
     const navbar = document.querySelector(".navbar");
@@ -34,4 +40,64 @@ const observer = new IntersectionObserver((entries) => {
 
 numbers.forEach((number) => {
     observer.observe(number);
+});
+
+// ================= PROTECTED FEATURES =================
+
+const protectedButtons = document.querySelectorAll(".card-btn");
+
+protectedButtons.forEach((button) => {
+
+    button.addEventListener("click", async (event) => {
+
+        const {
+            data: { user }
+        } = await supabase.auth.getUser();
+
+        if (!user) {
+            event.preventDefault();
+            loginModal.classList.add("show");
+        }
+
+    });
+
+});
+
+// ================= CONTINUE WITH GOOGLE =================
+
+continueGoogleBtn.addEventListener("click", async () => {
+
+    const { error } = await supabase.auth.signInWithOAuth({
+
+        provider: "google",
+
+        options: {
+            redirectTo: "http://127.0.0.1:5500/ui/home/home.html"
+        }
+
+    });
+
+    if (error) {
+
+        console.log(error);
+
+    }
+
+});
+
+// ================= CANCEL LOGIN =================
+
+cancelLoginBtn.addEventListener("click", () => {
+    loginModal.classList.remove("show");
+
+});
+
+// ================= CLOSE MODAL =================
+
+loginModal.addEventListener("click", (event) => {
+
+    if (event.target === loginModal) {
+        loginModal.classList.remove("show");
+    }
+
 });
